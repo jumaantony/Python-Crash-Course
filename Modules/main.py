39b6c0dc59adc739ac733a43501809e772ac3686
Juma Anton <47322942+Jumaantony@users.noreply.github.com>
2021-07-16 09:11:40 +0300

    python modules

---
+++ Modules/main.py
@@ -0,0 +1,6 @@
+import utility
+from shopping.shopping_cart import buy
+
+print(utility.divide(2, 3))
+
+print(buy('apple'))
---
+++ Modules/shopping/shopping_cart.py
@@ -0,0 +1,7 @@
+print(__name__)  # prints the name of the module when the module is called
+
+
+def buy(item):
+    cart = []
+    cart.append(item)
+    return cart
