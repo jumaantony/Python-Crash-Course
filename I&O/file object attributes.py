8af119f851c177ab0d3f97c5a25b91305259aac6
jumaantony <jumaantony840@gmail.com>
2023-01-05 08:01:17 +0300

    modules and I&O

--- I&O/file object attributes.py
+++ I&O/file object attributes.py
@@ -1,6 +1,9 @@
 # open file
 fo = open("input&output.py", "wb", 0)
+
 print("name of file: ", fo.name)
+
 print("Closed or not : ", fo.closed)
+
 print("Open mode: ", fo.mode)
 
--- Modules/shopping/shopping_cart.py
+++ Modules/shopping/shopping_cart.py
@@ -1,3 +1,7 @@
+# Consider a module to be the same as a code library.
+
+# A file containing a set of functions you want to include in your application
+
 print(__name__)  # prints the name of the module when the module is called
 
 
