7f57964a4ea0d891f5a053e8b0cfd162574d3112
jumaantony <jumaantony840@gmail.com>
2023-01-05 15:23:10 +0300

    decorators

--- Decorators/decorators.py
+++ Decorators/decorators.py
@@ -1,4 +1,7 @@
 # decorators
+# A function returning another function, usually applied as a function 
+# transformation using the @wrapper syntax.
+
 def my_decorator(func):
     def warp_func():
         print('***********')
@@ -16,19 +19,19 @@ def hello():
 hello()
 
 
-# decorators
-def my_decorator2(func):
-    def warp_func2(*args, **kwargs):
-        print('***********')
-        func(*args, **kwargs)
-        print('***********')
+# # decorators
+# def my_decorator2(func):
+#     def warp_func2(*args, **kwargs):
+#         print('***********')
+#         func(*args, **kwargs)
+#         print('***********')
 
-    return warp_func2
-
-
-@my_decorator2
-def hello2(greetings, emoji=':('):
-    print(greetings, emoji)
+#     return warp_func2
 
 
-hello2('Hiiiiii')
+# @my_decorator2
+# def hello2(greetings, emoji=':('):
+#     print(greetings, emoji)
+
+
+# hello2('Hiiiiii')
