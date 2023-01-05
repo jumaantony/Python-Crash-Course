0ffd3bc4fb91cf3654c4d6da24484b4b944f9d22
jumaantony <jumaantony840@gmail.com>
2023-01-05 08:05:03 +0300

    exceptions

--- Errors in Python/errors.py
+++ Errors in Python/errors.py
@@ -8,6 +8,18 @@ Types of errros
 3. Type Error
 4. Index error for list
 5. key error for dict
+6. Zero division error when a number is divided by zero.
+
+
+The try statement works as follows.
+
+First, the try clause (the statement(s) between the try and except keywords) is executed.
+
+If no exception occurs, the except clause is skipped and execution of the try statement is finished.
+
+If an exception occurs during execution of the try clause, the rest of the clause is skipped. Then, if its type matches the exception named after the except keyword, the except clause is executed, and then execution continues after the try/except block.
+
+If an exception occurs which does not match the exception named in the except clause, it is passed on to outer try statements; if no handler is found, it is an unhandled exception and execution stops with a message as shown above.
 """
 while True:
     try:
